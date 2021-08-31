2a03cc990f8a18efe82474771e9f73438c688bd7
cindy <283175003@qq.com>
2021-08-31 11:39:30 +0800

    update

---
+++ webpack.config.base.js
@@ -0,0 +1,16 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "index.[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "写代码了",
+      template: "src/assets/index.html",
+    }),
+  ],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,15 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
+const base = require("./webpack.config.base.js");
 module.exports = {
-  mode: "development",
+  mode: "production",
+  ...base,
   devtool: "inline-source-map",
   devServer: {
     contentBase: "./dist",
   },
-  entry: "./src/index.js",
-  output: {
-    filename: "index.[contenthash].js",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "写代码了",
-      template: "src/assets/index.html",
-    }),
-  ],
   module: {
     rules: [
       {
