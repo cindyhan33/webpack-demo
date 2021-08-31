ecce559f5f4113cd1cbbc4047f3258c85b4bd7c6
cindy <283175003@qq.com>
2021-08-31 23:06:03 +0800

    准备上传了

--- src/x.js
+++ src/x.js
@@ -1,2 +1,4 @@
-import css from "./x.css";
+import "./x.scss";
+import "./y.less";
+import "./z.styl";
 export default "xxx";
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -3,6 +3,15 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+  performance: {
+    hints: "warning", // 枚举 false关闭
+    maxEntrypointSize: 100000000, // 最大入口文件大小
+    maxAssetSize: 100000000, // 最大资源文件大小
+    assetFilter: function (assetFilename) {
+      //只给出js文件的性能提示
+      return assetFilename.endsWith(".js");
+    },
+  },
   entry: "./src/index.js",
   output: {
     filename: "index.[contenthash].js",
@@ -13,4 +22,33 @@ module.exports = {
       template: "src/assets/index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.styl$/i,
+        loader: ["style-loader", "css-loader", "stylus-loader"],
+      },
+      {
+        test: /\.less$/i,
+        loader: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("dart-sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
